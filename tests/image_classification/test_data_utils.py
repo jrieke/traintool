ae8add29b1350cfcd91e6b36ea4cd037d96140bb
Johannes Rieke <johannes.rieke@gmail.com>
2020-10-20 23:30:26 +0200

    Finished data conversion stuff

--- tests/image_classification/test_data_utils.py
+++ tests/image_classification/test_data_utils.py
@@ -10,38 +10,46 @@ from traintool.image_classification.data_utils import (
     recognize_data_format,
     torch_to_numpy,
     numpy_to_torch,
+    files_to_numpy,
+    files_to_torch,
 )
 
 
 @pytest.fixture
 def numpy_data():
-    return create_image_classification_data(data_format="numpy", seed=0)
+    return create_image_classification_data(
+        data_format="numpy", seed=0, grayscale=False
+    )
 
 
 @pytest.fixture
 def torch_data():
-    return create_image_classification_data(data_format="torch", seed=0)
+    return create_image_classification_data(
+        data_format="torch", seed=0, grayscale=False
+    )
 
 
 @pytest.fixture
 def files_data(tmp_path):
-    # TODO: Create some images in the dir.
-    return tmp_path
+    return create_image_classification_data(
+        data_format="files", seed=0, tmp_path=tmp_path
+    )
 
 
 def test_recognize_data_format(numpy_data, torch_data, files_data):
     assert recognize_data_format(numpy_data) == "numpy"
     assert recognize_data_format(torch_data) == "pytorch-dataset"
     assert recognize_data_format(files_data) == "files"
-    
+
     with pytest.raises(ValueError):
         recognize_data_format(None)
-        
+
     with pytest.raises(ValueError):
         recognize_data_format([1, 2, 3])
-    
+
     with pytest.raises(FileNotFoundError):
         recognize_data_format("non/existent/dir/123")
+
 
 def test_torch_to_numpy(numpy_data, torch_data):
     converted_data = torch_to_numpy(torch_data)
@@ -51,8 +59,34 @@ def test_torch_to_numpy(numpy_data, torch_data):
 
 def test_numpy_to_torch(numpy_data, torch_data):
     converted_data = numpy_to_torch(numpy_data)
-    assert torch.allclose(converted_data[0][0], torch_data[0][0])
+    # Note that we compare with tolerance of 0.1 here, because due to conversion to PIL,
+    # values are not exactly preserved.
+    assert torch.allclose(converted_data[0][0], torch_data[0][0], atol=0.1)
     assert converted_data[0][1] == torch_data[0][1]
 
+    resized_converted_data = numpy_to_torch(
+        numpy_data, resize=256, crop=224, mean=[0.1, 0.1, 0.1], std=[0.1, 0.1, 0.1]
+    )
+    assert resized_converted_data[0][0].shape[1] == 224
+    assert resized_converted_data[0][0].shape[2] == 224
+    
 
-# TODO: Write tests for to_torch and to_numpy.
+def test_files_to_numpy(files_data, numpy_data):
+    converted_data = files_to_numpy(files_data)
+    assert converted_data[0][0].shape == numpy_data[0][0].shape
+
+    resized_converted_data = files_to_numpy(
+        files_data, resize=256, crop=224, mean=[0.1, 0.1, 0.1], std=[0.1, 0.1, 0.1]
+    )
+    assert resized_converted_data[0][0].shape[1] == 224
+    assert resized_converted_data[0][0].shape[2] == 224
+    
+def test_files_to_torch(files_data, torch_data):
+    converted_data = files_to_torch(files_data)
+    assert converted_data[0][0].shape == torch_data[0][0].shape
+
+    resized_converted_data = files_to_numpy(
+        files_data, resize=256, crop=224, mean=[0.1, 0.1, 0.1], std=[0.1, 0.1, 0.1]
+    )
+    assert resized_converted_data[0][0].shape[1] == 224
+    assert resized_converted_data[0][0].shape[2] == 224
--- traintool/image_classification/data_utils.py
+++ traintool/image_classification/data_utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch
 from torch.utils.data import Dataset, TensorDataset
+from torchvision import datasets, transforms
 from typing import List, Tuple, Union
 from pathlib import Path
 
@@ -17,27 +18,41 @@ def channels_last(images: np.ndarray) -> np.ndarray:
 
 def recognize_data_format(data) -> str:
     """Returns a string which describes the data format of `data`."""
+
+    # Check for pytorch dataset.
     if isinstance(data, Dataset):
         return "pytorch-dataset"
 
+    # Check for iterable of numpy arrays (images, labels).
     try:
         if (
             len(data) == 2
             and isinstance(data[0], np.ndarray)
             and isinstance(data[1], np.ndarray)
         ):
-            return "numpy"
-    except TypeError:  # no iterable
+            # Check for correct shape of images.
+            images, labels = data
+            if len(images.shape) == 4 and (images.shape[1] in [1, 3]):
+                return "numpy"
+            else:
+                raise ValueError(
+                    "Shape of images not understood, should be "
+                    "num_samples x color_channels (1 or 3) x height x width, "
+                    f"is: {images.shape}"
+                )
+    except TypeError:  # data is no iterable
         pass
 
+    # Check for path to directory.
     try:
         if Path(data).exists():
             return "files"
         else:
-            raise FileNotFoundError(f"Data directory not found: {data}")
+            raise FileNotFoundError(f"Data directory does not exist: {data}")
     except TypeError:  # not a file or dir
         pass
 
+    # If all checks failed...
     raise ValueError(
         "Data format not recognized. Supported formats: list of numpy "
         "arrays, torch dataset, directory of image files"
@@ -46,38 +61,111 @@ def recognize_data_format(data) -> str:
     # TODO: Maybe add pytorch tensors.
 
 
-def to_torch(data) -> Dataset:
+def to_torch(data) -> Union[Dataset, None]:
     """Convert data from any format to torch datasets."""
-    data_format = recognize_data_format(data)
-    if data_format == "pytorch-dataset":
-        return data
-    elif data_format == "numpy":
-        return numpy_to_torch(data)
-
-
-def to_numpy(data) -> List[np.ndarray]:
-    """Convert data from any format to lists of numpy arrays [input, target]."""
-    data_format = recognize_data_format(data)
-    if data_format == "pytorch-dataset":
-        return torch_to_numpy(data)
-    elif data_format == "numpy":
-        return data
-
-
-def numpy_to_torch(data: List[np.ndarray]) -> Union[Dataset, None]:
-    """Convert data from list of numpy arrays [input, target] to torch datasets."""
 
     # Handle empty dataset.
     if data is None:
         return None
 
+    # Recognize data format and convert accordingly.
+    data_format = recognize_data_format(data)
+    if data_format == "pytorch-dataset":
+        return data
+    elif data_format == "numpy":
+        return numpy_to_torch(data)
+    elif data_format == "files":
+        return files_to_torch(data)
+    else:
+        raise RuntimeError()
+
+
+def to_numpy(data) -> Union[List[np.ndarray], None]:
+    """Convert data from any format to lists of numpy arrays [input, target]."""
+    # Handle empty dataset.
+    if data is None:
+        return None
+
+    # Recognize data format and convert accordingly.
+    data_format = recognize_data_format(data)
+    if data_format == "pytorch-dataset":
+        return torch_to_numpy(data)
+    elif data_format == "numpy":
+        return data
+    elif data_format == "files":
+        return files_to_numpy(data)
+    else:
+        raise RuntimeError()
+
+
+class ImageDataset(Dataset):
+    """
+    Generic dataset for images that can deal with PIL images, numpy arrays, 
+    or torch tensors.
+    """
+
+    def __init__(self, images, labels, transform=None):
+        self.images = images
+        self.labels = labels
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        image = self.images[idx]
+        label = self.labels[idx]
+
+        if self.transform:
+            image = self.transform(image)
+
+        return image, label
+
+
+def numpy_to_torch(
+    data: List[np.ndarray],
+    resize: int = None,
+    crop: int = None,
+    mean: List = None,
+    std: List = None,
+) -> Dataset:
+    """Convert data from list of numpy arrays [input, target] to torch datasets."""
     # Unpack arrays.
     images, labels = data
 
-    # Convert arrays to torch tensors and wrap in TensorDataset.
-    dataset = TensorDataset(
-        torch.from_numpy(images).float(), torch.from_numpy(labels).long(),
+    # Rescale images to 0-255 and convert to uint8.
+    # TODO: Right now this is done for each dataset individually, which is probably
+    #   not a big deal, because all datasets should usually contain values from the
+    #   complete range of values. Ideally though, this should probably be done based
+    #   on the values of the train dataset.
+    images = (images - np.min(images)) / np.ptp(images) * 255
+    images = images.astype(np.uint8)
+
+    # If images are grayscale, convert to RGB by duplicating channels.
+    if images.shape[1] == 1:
+        images = np.stack((images[:, 0],) * 3, axis=1)
+
+    # Convert to channels last format (required for transforms.ToPILImage).
+    images = channels_last(images)
+
+    # Set up transform to convert to PIL image, do manipulations, and convert to tensor.
+    # TODO: Converting to PIL and then to tensor is not super efficient, find a better
+    #   method.
+    transform = create_transform(
+        from_numpy=True, resize=resize, crop=crop, mean=mean, std=std
     )
+
+    # [0.485, 0.456, 0.406]
+    # [0.229, 0.224, 0.225]
+
+    # Convert labels to tensors.
+    labels = torch.from_numpy(labels).long()
+
+    # Construct dataset.
+    dataset = ImageDataset(images, labels, transform=transform)
     return dataset
 
 
@@ -98,3 +186,54 @@ def torch_to_numpy(data: Dataset) -> List[np.ndarray]:
         labels[i] = label
     return [images, labels]
 
+
+def files_to_numpy(
+    root: Path,
+    resize: int = None,
+    crop: int = None,
+    mean: List = None,
+    std: List = None,
+) -> List[np.ndarray]:
+    """Load image files into pytorch dataset and convert to numpy array."""
+    dataset = files_to_torch(root, resize=resize, crop=crop, mean=mean, std=std)
+    numpy_data = torch_to_numpy(dataset)
+    return numpy_data
+
+
+def files_to_torch(
+    root: Path,
+    resize: int = None,
+    crop: int = None,
+    mean: List = None,
+    std: List = None,
+) -> Dataset:
+    """Load image files into pytorch dataset."""
+    # Set up transform for loading and converting files.
+    # TODO: For the sklearn models, this probably shouldn't load in size 224,
+    #   especially in case the images are smaller.
+    transform = create_transform(resize=resize, crop=crop, mean=mean, std=std)
+
+    # Load images from folder.
+    dataset = datasets.ImageFolder(root, transform=transform)
+    return dataset
+
+
+def create_transform(
+    from_numpy: bool = False,
+    resize: int = None,
+    crop: int = None,
+    mean: List = None,
+    std: List = None,
+):
+    """Creates a torchvision transform to convert images from PIL or numpy."""
+    t = []
+    if from_numpy:
+        t.append(transforms.ToPILImage())
+    if resize is not None:
+        t.append(transforms.Resize(resize))
+    if crop is not None:
+        t.append(transforms.CenterCrop(crop))
+    t.append(transforms.ToTensor())
+    if mean is not None and std is not None:
+        t.append(transforms.Normalize(mean=mean, std=std),)
+    return transforms.Compose(t)
